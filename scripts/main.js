5d2008c9ba3093e109651dc47f81b9d128c2c8b1
ShayasTech <shayas.tech@gmail.com>
2022-06-03 13:19:44 +0300

    Project Compleated

---
+++ scripts/main.js
@@ -0,0 +1,31 @@
+let password = document.querySelector("#password");
+let confirmPassword = document.querySelector("#confirm-pass");
+let confirmMsg = document.querySelector(".password-match-msg");
+
+confirmPassword.addEventListener('keyup', ()=>{
+    if(password.value == confirmPassword.value){
+        confirmMsg.innerText = "Password Match";
+        confirmMsg.style.color = "green";
+        password.style.border = "1px solid green";
+        confirmPassword.style.border = "1px solid green";
+    }else{
+        confirmMsg.innerText = "* Password do not match";
+        confirmMsg.style.color = "red";
+        password.style.border = "1px solid red"
+        confirmPassword.style.border = "1px solid red"
+    }
+});
+
+password.addEventListener('keyup', ()=>{
+    if(password.value == confirmPassword.value){
+        confirmMsg.innerText = "Password Match";
+        confirmMsg.style.color = "green";
+        password.style.border = "1px solid green";
+        confirmPassword.style.border = "1px solid green";
+    }else{
+        confirmMsg.innerText = "* Password do not match";
+        confirmMsg.style.color = "red";
+        password.style.border = "1px solid red"
+        confirmPassword.style.border = "1px solid red"
+    }
+});
